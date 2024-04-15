94f013e946c1dda3426fb5c4343f4085d1b5a796
Zidan Abraham <zidangit07@gmail.com>
2024-04-15 19:37:30 +0800

    feat: change spacing to 4pt grid in Hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,54 +5,27 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
     fontSize: {
-      "2xs": [
-        "10px",
-        {
-          lineHeight: "auto",
-        },
-      ],
-      xs: [
-        "12px",
-        {
-          lineHeight: "auto",
-        },
-      ],
-      sm: [
-        "14px",
-        {
-          lineHeight: "140%",
-        },
-      ],
-      base: [
-        "16px",
-        {
-          lineHeight: "140%",
-        },
-      ],
-      lg: [
-        "26px",
-        {
-          lineHeight: "120%",
-        },
-      ],
-      xl: [
-        "42px",
-        {
-          lineHeight: "110%",
-        },
-      ],
-      "2xl": [
-        "68px",
-        {
-          lineHeight: "110%",
-        },
-      ],
-      "3xl": [
-        "110px",
-        {
-          lineHeight: "110%",
-        },
-      ],
+      "2xs": ["4px", { lineHeight: "auto" }],
+      xs: ["8px", { lineHeight: "auto" }],
+      sm: ["12px", { lineHeight: "140%" }],
+      base: ["16px", { lineHeight: "140%" }],
+      lg: ["20px", { lineHeight: "120%" }],
+      xl: ["24px", { lineHeight: "120%" }],
+      "2xl": ["28px", { lineHeight: "120%" }],
+      "3xl": ["32px", { lineHeight: "110%" }],
+      "4xl": ["36px", { lineHeight: "110%" }],
+      "5xl": ["40px", { lineHeight: "110%" }],
+      "6xl": ["44px", { lineHeight: "110%" }],
+      "7xl": ["48px", { lineHeight: "110%" }],
+      "8xl": ["52px", { lineHeight: "110%" }],
+      "9xl": ["56px", { lineHeight: "110%" }],
+      "10xl": ["64px", { lineHeight: "110%" }],
+      "11xl": ["72px", { lineHeight: "110%" }],
+      "12xl": ["80px", { lineHeight: "110%" }],
+      "13xl": ["88px", { lineHeight: "110%" }],
+      "14xl": ["96px", { lineHeight: "110%" }],
+      "15xl": ["112px", { lineHeight: "110%" }],
+      "16xl": ["128px", { lineHeight: "110%" }],
     },
     extend: {
       screens: {
